b9b3324ceffa60f1569a78e1f245f056b5ab1aee
jame3032002 <jame3032002@hotmail.com>
2021-09-14 00:04:37 +0700

    :tada: test nextjs env

---
+++ frontend/next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  serverRuntimeConfig: {
+    BACKEND_SERVICE_URL: process.env.BACKEND_SERVICE_URL,
+    KAJAME: 'NAJA'
+  },
+  publicRuntimeConfig: {
+    BACKEND_DOMAIN_URL: process.env.BACKEND_DOMAIN_URL
+  }
+}
---
+++ frontend/pages/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import getConfig from 'next/config'
+
+const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
+
+function IndexPage () {
+  console.log('Run on Client: BACKEND_DOMAIN_URL =>', publicRuntimeConfig?.BACKEND_DOMAIN_URL)
+  console.log('Run on Server: BACKEND_SERVICE_URL =>', serverRuntimeConfig?.BACKEND_SERVICE_URL)
+  console.log('Run on Server: KAJAME =>', serverRuntimeConfig?.KAJAME)
+  console.log('process env BACKEND_DOMAIN_URL =>', process.env.BACKEND_DOMAIN_URL)
+
+  return (
+    <div>IndexPage</div>
+  )
+}
+
+export async function getServerSideProps (context) {
+  return {
+    props: {}
+  }
+}
+
+export default IndexPage
